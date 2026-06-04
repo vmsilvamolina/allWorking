6e4167a3ceb35add9aeaf8d41733c50e96a50105
Victor Silva <vmsilvamolina@gmail.com>
2026-06-04 18:48:26 -0300

    update app

--- app/main.go
+++ app/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		hostname, _ := os.Hostname()
-		fmt.Fprintf(w, "<h1>Hello from ECS!</h1><p>Host: %s</p>", hostname)
+		fmt.Fprintf(w, "<h1>Hello from ECS! (v1)</h1><p>Host: %s</p>", hostname)
 	})
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
